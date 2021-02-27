fe689e59a400023f9683798fa46e9121ac53594f
Santiago Maceri <santimaceri@gmail.com>
2021-02-27 10:17:30 -0300

    changed port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+const port = 3000;
 const https = require("https");
 const cheerio = require("cheerio");
 const csv = require("csv-parser");
